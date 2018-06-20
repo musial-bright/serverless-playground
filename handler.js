d3f99baefd0304047f2086dfc8ba9dd0b32bbbe0
Adam Musial-Bright <adam.musialbright@googlemail.com>
2018-06-20 14:17:30 +0200

    load works

--- handler.js
+++ handler.js
@@ -1,19 +1,58 @@
 'use strict';
 
-module.exports.info = (event, context, callback) => {
-  let name = event.name;
-  let message = `Hello ${name}. Welcome to Lambda.`;
+const AWS = require('aws-sdk');
+
+class Storage {
+
+  constructor() {
+    this.config = {
+      s3: { Bucket: "adams-serverless-files", Key: "store.json"}
+    }
+  }
+
+  load(callback) {
+    var s3 = new AWS.S3();
+    s3.getObject(this.config.s3, (err, data) => {
+      if (err) {
+        console.err(err);
+      } else {
+        callback(data.Body.toString());
+      }
+    });
+  }
+
+  save(data) {
+
+  }
+}
+
+
+module.exports.load = (event, context, callback) => {
+  let storage = new Storage();
+  storage.load((data) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: data,
+        input: event,
+      }),
+    };
+
+    callback(null, response);
+  });
+};
+
+module.exports.save = (event, context, callback) => {
+  let storage = new Storage();
+  storage.save(event.data);
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: message,
+      message: "saved",
       input: event,
     }),
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
