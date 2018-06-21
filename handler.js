fda454df8efb11d51449a6386efff43680fc37d4
Adam Musial-Bright <adam.musialbright@googlemail.com>
2018-06-21 10:42:22 +0200

    simplified responses

--- handler.js
+++ handler.js
@@ -5,41 +5,21 @@ const Storage = require("./services/storage");
 module.exports.load = (event, context, callback) => {
   let storage = new Storage(process.env.s3Bucket, 'store.json');
   storage.load().then(data => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: data,
-        input: event,
-      }),
-    };
-
+    const response = { statusCode: 200, body: JSON.stringify({ message: data }) };
     callback(null, response);
   }).catch(error => {
     console.error(error)
-    callback(null, {
-      statusCode: 404,
-      body: null
-    })
+    callback(null, { statusCode: 404, body: null })
   });
 };
 
 module.exports.save = (event, context, callback) => {
   let storage = new Storage(process.env.s3Bucket, 'store.json');
   storage.save(event).then(s3response => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "saved",
-        input: event,
-      }),
-    };
-
+    const response = { statusCode: 200, body: null };
     callback(null, response);
   }).catch(error => {
     console.error(error)
-    callback(null, {
-      statusCode: 422,
-      body: null
-    })
+    callback(null, { statusCode: 422, body: null });
   });
 };
